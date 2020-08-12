d1db8fd1feb92e91e7d25e6f3648735a5349d470
Sampth Sandaruwan <smcmails@gmail.com>
2020-08-12 22:43:40 +0530

    Version 1.0.2 published

---
+++ dist/smc-query-builder.helper.d.ts
@@ -0,0 +1,12 @@
+declare class SMCQueryBuilderHelper {
+    valueMatcher(filter: any, key: string, value: any): any;
+    objectIdMatcher(filter: any, key: string, value: string): any;
+    patternMatcher(filter: any, key: string, value: any): any;
+    patternUnMatcher(filter: any, key: string, value: any): any;
+    orOperator(filter: any, valuesArray: any[]): any;
+    andOperator(filter: any, valuesArray: any[]): any;
+    arrayElementMatcher(filter: any, key: string, valuesArray: any[]): any;
+    numberPatternMatcher(filter: any, key: string, value: string): any;
+}
+declare const _default: SMCQueryBuilderHelper;
+export default _default;
---
+++ dist/smc-query-builder.helper.js
@@ -0,0 +1,71 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = require("mongoose");
+var SMCQueryBuilderHelper = /** @class */ (function () {
+    function SMCQueryBuilderHelper() {
+    }
+    SMCQueryBuilderHelper.prototype.valueMatcher = function (filter, key, value) {
+        if (value || value === false) {
+            filter[key] = value;
+        }
+        return filter;
+    };
+    SMCQueryBuilderHelper.prototype.objectIdMatcher = function (filter, key, value) {
+        if (value) {
+            try {
+                filter[key] = mongoose_1.Types.ObjectId(value);
+            }
+            catch (e) { }
+        }
+        return filter;
+    };
+    SMCQueryBuilderHelper.prototype.patternMatcher = function (filter, key, value) {
+        if (value) {
+            filter[key] = { $regex: new RegExp(value, 'i') };
+        }
+        return filter;
+    };
+    SMCQueryBuilderHelper.prototype.patternUnMatcher = function (filter, key, value) {
+        if (value) {
+            filter[key] = { $not: new RegExp(value, 'i') };
+        }
+        return filter;
+    };
+    SMCQueryBuilderHelper.prototype.orOperator = function (filter, valuesArray) {
+        if (valuesArray && valuesArray.length > 0) {
+            filter['$or'] = valuesArray;
+        }
+        return filter;
+    };
+    SMCQueryBuilderHelper.prototype.andOperator = function (filter, valuesArray) {
+        if (valuesArray && valuesArray.length > 0) {
+            filter['$and'] = valuesArray;
+        }
+        return filter;
+    };
+    SMCQueryBuilderHelper.prototype.arrayElementMatcher = function (filter, key, valuesArray) {
+        if (valuesArray && valuesArray.length > 0) {
+            filter[key] = { $in: valuesArray };
+        }
+        return filter;
+    };
+    SMCQueryBuilderHelper.prototype.numberPatternMatcher = function (filter, key, value) {
+        if (value) {
+            if (!filter.$expr) {
+                filter.$expr = {};
+            }
+            if (!filter.$expr.$and) {
+                filter.$expr.$and = [];
+            }
+            filter.$expr.$and.push({
+                $regexMatch: {
+                    input: { $toString: "$" + key },
+                    regex: new RegExp(value, 'i')
+                }
+            });
+        }
+        return filter;
+    };
+    return SMCQueryBuilderHelper;
+}());
+exports.default = new SMCQueryBuilderHelper();
